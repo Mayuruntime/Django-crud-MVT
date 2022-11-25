ce3ea7fb73975d9e3548ca1f5827288417ada5a7
Mayuruntime <mayur.runtime@gmail.com>
2022-11-25 16:15:10 +0530

    hello commit

--- employee/views.py
+++ employee/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render, redirect, HttpResponse
 from .models import Employee
 from .forms import EmployeeForm
 
-# from django.core.mail import send_mail
-# from main.settings import DATABASES, EMAIL_HOST_USER, TIME_ZONE
-# import requests
-# import smtplib , ssl
-
 def employees_list(request):
     employees = Employee.objects.filter(is_deleted=False,is_active=True)
     context = {
@@ -47,28 +42,6 @@ def edit_employee(request, id):
 
 
 def delete_employee(request, id):
-
-    # message = f"""Runtime Solution Welcomes You \n\n USERNAME:  "Mayur" \n PASSWORD:  "helloMayur" """
-    # recepient = "mayur.runtime@gmail.com"     
-    # # send_mail(subject, message, EMAIL_HOST_USER, [recepient], fail_silently = False)  
-
-    # url = "http://127.0.0.1:8000/"
-    # payload={}
-    # headers = {}
-    # response = requests.request("GET", url, headers=headers, data=payload)
-    # while True:
-    #     # sending mail
-    #     server = smtplib.SMTP("smtp.falconide.com",587)
-    #     server.ehlo()
-    #     server.starttls
-    #     server.login("runtimefal","Runtime@2022")
-    #     server.sendmail("noreply@runtime-solutions.com" , recepient , message)
-    #     server.quit()
-    #     # checkAPiServerAvilableOrNot()
-    #     # time.sleep(1000)       
-    #     break 
-
-
     employee = Employee.objects.get(id=id)
 
     if request.method == 'POST':
--- main/settings.py
+++ main/settings.py
@@ -123,13 +123,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# # EMAIL_HOST_USER = 'runtimefal'
-# # EMAIL_HOST_PASSWORD = 'Runtime@2022'
-# # EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
