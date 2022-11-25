5e314f41f58e84aa7b8bba93b96d36d3b1d64500
Mayuruntime <mayur.runtime@gmail.com>
2022-11-25 13:37:17 +0530

    mayur commit one

--- employee/urls.py
+++ employee/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -6,4 +8,4 @@ urlpatterns = [
     path('create/', views.create_employee, name='create-employee'),
     path('edit/<int:id>/', views.edit_employee, name='edit-employee'),
     path('delete/<int:id>/', views.delete_employee, name='delete-employee'),
-]
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
